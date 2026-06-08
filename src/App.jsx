64030de93c683ff2bed9d9261cc2ec653e2625a2
Nicole-favela <favela.nicoleerin@gmail.com>
2026-06-08 14:45:58 -0600

    simple usestate now adds session storage for boot screen animatation to prevent redoing on reload

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,29 @@ import BootScreen from './components/BootScreen/BootScreen.jsx'
 import Terminal from './components/Terminal/Terminal.jsx'
 import Desktop from './components/Desktop/Desktop.jsx'
 
+const alreadyBooted = sessionStorage.getItem('booted') === 'true' //uses the session storage to determine whether to show boot screen or skip to desktop so only closing tab and reopening shows boot screen again
 export default function App() {
-  const [phase, setPhase] = useState('boot')
+  const [phase, setPhase] = useState(alreadyBooted ? 'desktop' : 'boot')
+  
+  function handleBootComplete() {
+    sessionStorage.setItem('booted', 'true')
+    setPhase('terminal') //set next phase to see
+  }
+
+  function handleTerminalComplete() {
+    setPhase('desktop') //swap to desktop view after terminal
+  }
 
   return (
     <>
       <div className="scanlines" aria-hidden="true" />
 
       {phase === 'boot' && (
-        <BootScreen onComplete={() => setPhase('terminal')} />
+        <BootScreen onComplete={handleBootComplete} />
       )}
 
       {phase === 'terminal' && (
-        <Terminal onComplete={() => setPhase('desktop')} />
+        <Terminal onComplete={handleTerminalComplete} />
       )}
 
       {phase === 'desktop' && (

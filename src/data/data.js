805fb8f6f53e7eed7e598e274f08529a384d3866
Nicole-favela <favela.nicoleerin@gmail.com>
2026-06-08 16:25:04 -0600

    update to project data

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,5 @@
 export const USER_INFO = {
-  name:     'Nicole Favela',
+  name:     'Nicole',
   role:     'Full Stack Engineer',
   location: 'El Paso , TX',
   
@@ -31,18 +31,18 @@ export const PROJECTS = [
     description: 'An AI powered red team assistant using MITRE CALDERA that allows for automated execution, monitoring, reporting, and analysis of red team operations through a CLI.',
     motivation: 'This was born out of a desire to make red team operations more efficient and accessible, and to explore the intersection of AI and cybersecurity.',
     tech: ['Python', 'MITRE CALDERA API', 'Llama 3.1'],
-    github: 'https://github.com/',
-    demo: 'https://project-alpha.vercel.app',
+    github: 'https://github.com/Nicole-favela/caldera-shell',
+    demo: null,
     status: 'live',
     img: null
   },
   {
     id: '2',
     name: 'Self-RAG Research Project',
-    description: 'Another project',
-    motivation: 'This project was motivated by a desire to explore the capabilities of Retrieval Augmented Generation (RAG) and the possible improvements that could make responses more accurate and contextually relevant.',
-    tech: ['Python', 'FastAPI', 'Docker'],
-    github: 'https://github.com/yourusername/project-beta',
+    description: 'A graduate school level project that applies a lightweight self-RAG implementation based heavily on a research paper from 2023 to improve on traditional dumb retrieval RAG.',
+    motivation: 'This project was motivated by a desire to explore the capabilities of Retrieval Augmented Generation (RAG) and the possible improvements that could make responses more accurate and contextually relevant especially in fields such as finance or medicine where correctness is crucial.',
+    tech: ['Python', 'FAISS', 'Ollama', 'Llama 3.1', 'all-MiniLM-L6-v2 embedding model'],
+    github: 'https://github.com/Nicole-favela/dl-self-rag-project',
     demo: null,
     status: 'live',
     img: null
@@ -53,11 +53,33 @@ export const PROJECTS = [
     description: 'A reinforcement learning trained agent to play pong using Atari in gymnasium.',
     motivation: 'This project was motivated by the desire to apply reinforcement learning and specifically Q-learning to classic atari games to explore the capabilities and limitations.',
     tech: ['Pytorch', 'Q-learning', 'Matplotlib', 'Gymnasium'],
-    github: 'https://github.com/',
-    demo: null,
+    github: 'https://github.com/Nicole-favela/reinforcement-learning-arcade',
+    demo: 'https://www.youtube.com/watch?v=z04-U1CQrXk',
     status: 'live',
     img: null
   },
+   {
+    id: '4',
+    name: 'Spotify clone application',
+    description: 'Listen to music, view playlists, music library, even search by song/artist. Built using React, Node, spotify API, spotify-web-api-node, and MUI components.',
+    motivation: 'This was a passion project was motivated by my love of music and the desire to apply my full stack engineering skills to build a functional spotify clone taylored to my music library.',
+    tech: ['React', 'Node js', 'MUI', 'spotify API', 'spotify-web-api-node'],
+    github: 'https://github.com/Nicole-favela/SpotifyClone',
+    demo: 'https://www.dropbox.com/scl/fi/61yxjbnc5uqddot3z29jp/spotify_clone_demo.MP4?rlkey=svn3wpsaw2afvumwf3sx9nmb2&e=1&dl=0',
+    status: 'live',
+    img: null
+  },
+  {
+    id: '5',
+    name: 'Full Stack Netflix Clone',
+    description: `built with the MERN stack and incorporates user authentication using JWT and cookies for a seamless login and registration experience. Leveraging the TMDb API, the application mirrors the Netflix layout and design while utilizing React Player to stream movie trailers. The platform keeps track of users' recently viewed trailers and allows them to curate a personalized 'My List' with the ability to add or remove movies. Additionally, users can access their account details and log out through a dedicated profile page.`,
+    motivation: 'The motivation behind this was to create another fully deployed and functional full stack application that incorporates my love of movies and tv.',
+    tech: ['React', 'Node js', 'MUI', 'TMDb API', 'Mongodb', 'React Player', 'JWT', 'Cookies'],
+    github: 'https://github.com/Nicole-favela/Netflix-clone',
+    demo: 'https://streammoviesapp.onrender.com/login',
+    status: 'live',
+    img: null
+  }
 ]
 
 export const ACHIEVEMENTS = [
